f93fa18a0b8c55d7b7a4e8ea088d25660d130671
anhtuan0702 <tuan070200@gmail.com>
2022-06-08 23:24:10 +0700

    removed welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -1,9 +1,10 @@
 from tkinter import Tk, Label
+from tkinter.font import Font
 
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
 
-label  = Label(window, text = "Welcome!", font =("Arial Black", 78, "bold"), bg= "steelblue", fg= "white")
+label  = Label(window, font = ("Arial Black", 78, "bold"), bg= "steelblue", fg= "white")
 label.pack(pady= 100)
 window.mainloop()
