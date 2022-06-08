42a4c98c83aba2ada93e02b5f225d6f69989c318
anhtuan0702 <tuan070200@gmail.com>
2022-06-08 23:20:03 +0700

    added a welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -4,4 +4,6 @@ window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
 
+label  = Label(window, text = "Welcome!", font =("Arial Black", 78, "bold"), bg= "steelblue", fg= "white")
+label.pack(pady= 100)
 window.mainloop()
